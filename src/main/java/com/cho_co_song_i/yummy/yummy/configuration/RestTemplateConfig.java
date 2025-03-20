0d314788f06710ac172d5489efd094141671bd70
secretm <secretm224@gmail.com>
2025-03-20 17:31:59 +0900

    login system

---
+++ src/main/java/com/cho_co_song_i/yummy/yummy/configuration/RestTemplateConfig.java
@@ -0,0 +1,4 @@
+package com.cho_co_song_i.yummy.yummy.configuration;
+
+public class RestTemplateConfig {
+}
---
+++ src/main/java/com/cho_co_song_i/yummy/yummy/model/KakaoToken.java
@@ -0,0 +1,4 @@
+package com.cho_co_song_i.yummy.yummy.model;
+
+public class KakaoToken {
+}
