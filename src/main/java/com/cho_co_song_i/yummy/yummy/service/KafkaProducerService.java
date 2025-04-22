8a3122e27619e62a5f9343fbb3ed5a93ee03fbf7
MoonHoSeok <secretm224@gmail.com>
2025-04-22 14:28:31 +0900

    masrer branch error edit

--- src/main/java/com/cho_co_song_i/yummy/yummy/service/KafkaProducerService.java
+++ src/main/java/com/cho_co_song_i/yummy/yummy/service/KafkaProducerService.java
@@ -1,20 +1,18 @@
 package com.cho_co_song_i.yummy.yummy.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaProducerService {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    @Value("${spring.topic.kafka.find-user-info}")
-    private String topicName;
-
-    public void sendMessage(String message) {
-        kafkaTemplate.send(topicName, message);
-    }
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    @Value("${spring.topic.kafka.find-user-info}")
+//    private String topicName;
+//
+//    public void sendMessage(String message) {
+//        kafkaTemplate.send(topicName, message);
+//    }
 }
