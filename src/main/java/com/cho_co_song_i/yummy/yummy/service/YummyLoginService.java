2283050ab375e2e4846ccd1106399b64f989ff70
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-05-07 13:32:17 +0900

    긴급에러 수정
    긴급에러 수정

--- src/main/java/com/cho_co_song_i/yummy/yummy/service/YummyLoginService.java
+++ src/main/java/com/cho_co_song_i/yummy/yummy/service/YummyLoginService.java
@@ -249,6 +249,7 @@ public class YummyLoginService {
     private Optional<UserBasicInfoDto> fetchUserProfileFromRedis(String userNo) {
         String keyPrefix = String.format("%s:%s", userInfoKey, userNo);
         UserBasicInfoDto userDto = redisService.getValue(keyPrefix, new TypeReference<UserBasicInfoDto>() {});
+        log.info("=============================================");
         log.info("userDto: {}", userDto);
 
         return Optional.ofNullable(userDto);
